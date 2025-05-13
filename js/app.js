7a6e2d2a7cc51154f6236b22096e2e3a41bce897
nelicepriatniy <92029297+nelicepriatniy@users.noreply.github.com>
2025-05-13 21:39:28 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const heroSlider = new Swiper('.hero-bg', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.hero-bg-pagination',
+        clickable: true,
+    },
+});
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+    slidesPerView: 3,
+    spaceBetween: 33,
+    loop: true,
+    navigation: {
+        nextEl: '.reviews__wrapper-arrow-next',
+        prevEl: '.reviews__wrapper-arrow-prev',
+    },
+});
+
+
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        header.classList.add('header--scrolled');
+    } else {
+        header.classList.remove('header--scrolled');
+    }
+});
